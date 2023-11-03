c6e38930939f232649354f9ecb1c1e9908f70827
smishra1000 <smishra1000@rediffmail.com>
2023-11-03 19:49:52 +0530

    js sesiion operator done

---
+++ js-session/operator.js
@@ -0,0 +1,87 @@
+// 1. Airthmatical operator
+ //+,-,*,/%
+
+//  let a = 10;
+//  let b = 20;
+
+//  console.log(a+b)
+//  console.log(a-b)
+//  console.log(a*b)
+//  console.log(a/b)
+//  console.log(a%b)
+
+
+ // unary operator 
+//  console.log(-a)
+ // increment operator 
+//  let x = 10;
+//  console.log(x++)
+//  console.log(++x)
+//  let a = 10
+//  let b = ++a;
+//  console.log(a,b)
+
+ // pre decrement post decrement
+
+//  let x = 5;
+//  let y = x--
+//  console.log(x,y)
+
+
+ // Realtional operator/Comparision operator
+ //1. == 
+
+//  let a = 7
+//  let b = "7"
+//  console.log(a==b)
+
+//  let a = 8
+//  let b = "8"
+
+//  console.log(a===b)
+
+ //==  === > < >= <= !=
+
+//  let a = 5
+//  let b = 9
+//  console.log(a<=b)
+
+// let a = 6
+// let b = 6
+// console.log(b>=a)
+
+// let a = 10
+// let b = 10
+// console.log(a!=b)
+
+// Logical operator 
+// && || !
+
+
+// let a = 3 
+// let b = 4
+// let c= 5
+
+// console.log(a>b && c>a)// false
+// console.log((b>a && a>c) || b>c)// false
+
+//! 
+//  console.log(!true)
+//  console.log(!false)
+
+//ternary operator 
+
+// let isPaid = false;
+// let canWatch = isPaid ? "yes" : "no"
+// console.log(canWatch)
+
+// let num = 7;
+// let result = num%2===0 ? "even number"  : " odd number"
+
+// console.log(result)
+
+// let age = 14
+// let isHeVoted = false
+
+// let result = age>18 ? isHeVoted ? "yes " : "no" : "not a valid voter"
+// console.log(result)
