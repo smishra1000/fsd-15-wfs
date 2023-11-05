9e7c614f704a36f98d100def07955e7f2aa1b3d0
smishra1000 <smishra1000@rediffmail.com>
2023-11-05 12:54:07 +0530

    function topic started

---
+++ js-session/function.js
@@ -0,0 +1,16 @@
+// let a = 6;
+// let b = 7;
+// console.log(a+b)
+
+// let x=7;
+// let y=9;
+// console.log(x+y)
+
+// function add() {
+//     let a = Number(prompt("enter first number"))
+//     let b = Number(prompt("enter second number"))
+//     console.log(`sum of 2 number=${a+b}`)
+// }
+
+// add();
+// add();
