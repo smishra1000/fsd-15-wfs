cd0a61f03099a1d6672a2c60f1c26f329c1c4028
smishra1000 <smishra1000@rediffmail.com>
2024-01-30 19:48:02 +0530

    todo app delete and update status code added

--- todo-app-react-node-express/backend/routes/todo.js
+++ todo-app-react-node-express/backend/routes/todo.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 
 const todos = [
+
 ]
 
 router.get("/list", function (req, res) {
@@ -12,8 +13,28 @@ router.get("/list", function (req, res) {
 
 router.post("/create",function(req,res){
     console.log(req.body)
-    todos.push({taskDesc:req.body.taskDesc,taskName:req.body.taskName,id:todos.length ? todos.length+1 : 1})
+    todos.push({taskDesc:req.body.taskDesc,taskName:req.body.taskName,id:todos.length ? todos.length+1 : 1,status:"inprogress"})
     res.send("create todo comming soon")
+})
+
+router.delete("/delete/:id",function(req,res){
+    console.log(req.params); //{id:2}
+
+    let filteredTodos = todos.filter((item)=>{
+        return item.id!==Number(req.params.id)
+    })
+    res.send(filteredTodos)
+
+})
+
+router.put("/update/:id",function(req,res){
+    let index = todos.findIndex((item)=>{
+        return item.id===Number(req.params.id)
+    })
+    if(index>-1){
+        todos[index].status="completed"
+    }
+    res.send(todos)
 })
 
 
--- todo-app-react-node-express/frontend/src/components/ToDoList.js
+++ todo-app-react-node-express/frontend/src/components/ToDoList.js
@@ -3,13 +3,39 @@ import { useState, useEffect } from "react"
 function ToDoList() {
     const [todos, setTodos] = useState([])
 
-    useEffect(() => {
+    const getAllToDos = ()=>{
         fetch("http://localhost:7000/todo/list").then(function (res) {
             return res.json();
         }).then(function (result) {
             setTodos(result)
         })
+    }
+
+    useEffect(() => {
+        getAllToDos();
     },[])
+
+    const deleteTodo = (id)=>{
+        // http://localhost:7000/todo/delete/2
+        fetch("http://localhost:7000/todo/delete/"+id,{method:"DELETE"}).then(function (res) {
+            return res.json();
+        }).then(function (result) {
+            // getAllToDos();
+            setTodos(result)
+        })
+    }
+
+    const taskDone = (id)=>{
+        fetch("http://localhost:7000/todo/update/"+id,{method:"PUT",headers: {
+            "Content-Type": "application/json",
+          },body:JSON.stringify({status:"completed"})}).then(function (res) {
+            return res.json();
+        }).then(function (result) {
+    
+            setTodos(result)
+        })
+    }
+
     return (
         <div className="container">
             {todos.length===0&&<h4>No Todos available</h4>}
@@ -22,15 +48,20 @@ function ToDoList() {
                             <th>ID</th>
                             <th>Task Name</th>
                             <th>Task Description</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {todos.map((item) => {
                             return (
-                                <tr>
+                                <tr className={item.status==="completed" ? 'line-through':''}>
                                     <td>{item.id}</td>
                                     <td>{item.taskName}</td>
                                     <td>{item.taskDesc}</td>
+                                    <td><span class="badge bg-warning text-dark">{item.status}</span></td>
+                                    <td><button className="btn btn-primary" onClick={()=>deleteTodo(item.id)}>Delete</button>
+                                    {item.status==="completed" ? null : <button className="btn btn-success " onClick={()=>taskDone(item.id)}>Done</button>}</td>
                                 </tr>
                             )
                         })}
