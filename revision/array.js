0a7a8cb2019aa80b09d26e340a3de6639447bc1d
smishra1000 <smishra1000@rediffmail.com>
2024-04-03 18:54:50 +0530

    revision added

---
+++ revision/array.js
@@ -0,0 +1,159 @@
+// var a=10;
+// let age=30
+// let colors = ["red","blue","pink","green"]
+// console.log(colors[0])
+// console.log(colors[1])
+
+// for(let i=0;i<colors.length;i++){
+// console.log(colors[i])
+// }
+
+// let j = 0
+// while(j<colors.length){
+//     console.log(colors[j])
+//     j++;
+// }
+
+// push,filter map reduce find findIndex,indexOf,splice
+
+// let str = "sameer"
+
+// let obj = {
+//     "name":ReadableStreamBYOBReader
+// }
+
+// for(let i in obj){
+//     console.log(i)
+// }
+
+// for(let i of str){
+//     console.log(i)
+// }
+
+
+// let user = {
+//     name:"sameer",
+//     age:20,
+//     ismarried:true
+// }
+
+// for(let key in user){
+//     console.log(user[key])
+// }
+
+// function sum(){
+
+// }
+
+// console.log("hello world")
+
+// console.log("good")
+
+// function greet(msg){
+//     console.log(msg)
+//     return 10;
+// }
+
+// let r=greet("hello")
+
+// console.log(r)
+
+
+// let userHasPaid = true;
+// function checkUserForHotstar(){
+//     if(userHasPaid===true){
+//         return "premium user"
+//     }else{
+//         return "free user"
+//     }
+// }
+
+
+// let result = checkUserForHotstar();
+// console.log(result)
+
+// let arr = ["sameer",10,true]
+
+// let users = [{},{},{}]
+
+// let employees = [
+//     {
+//         id:1,
+//         name:"sameer",
+//         age:30
+//     },
+//     {
+//         id:2,
+//         name:"test",
+//         age:30
+//     },
+//     {
+//         id:3,
+//         age:29,
+//         name:"test2"
+//     }
+// ]
+
+//1 find those employess which age is =30
+
+// let data = employees.filter(function(item){
+//     return item.age===30
+// })
+
+// console.log(data)
+
+//2 find ages of all employess
+
+// let ages = employees.map(function(item){
+//     return item.age
+// })
+
+// console.log(ages)
+
+
+// variables datatypes ongotpointercapture,
+// loop function ,array 
+// promise async await callback 
+// map filter find
+
+
+
+// let obj = {}---object literal
+// let user =  new Object();//Using Object constructor
+// function User(){
+//     this.name=name
+// }
+
+// let u = new User()
+
+// function greet(){
+//     console.log("hello")
+// }
+
+// let g1 = greet();
+
+// let g2 = new greet();
+// g2.age=20
+
+// console.log(g1)
+// console.log(g2)
+
+// Object.create() 
+// Object.assign()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
